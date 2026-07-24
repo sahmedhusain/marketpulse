6c95e36dda30d97b8d51c4049e31d29c6f99d717
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2026-07-24 12:36:10 +0300

    feat(auth): implement Devise RegistrationsController with permitted parameters

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,11 @@
-# This must be implemented.
-# sign_up_params and account_update_params
 class RegistrationsController < Devise::RegistrationsController
+  private
 
- private
- 
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
 end
